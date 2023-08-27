0100b1cff27f204810ea32a14d49bdefc88aebb0
Ashwani Kumar <130587125+ashwanimaurya85@users.noreply.github.com>
2023-08-27 07:06:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+            const form = document.getElementById('myForm');
+            const nameInput = document.getElementById('name');
+            const ageInput = document.getElementById('age');
+            const btn = document.getElementById('btn');
+ 
+            form.addEventListener('submit', function (event) {
+                event.preventDefault();
+ 
+                const name = nameInput.value;
+                const age = parseInt(ageInput.value);
+ 
+                if (!name || !age) {
+                    alert('Please fill out all fields.');
+                    return;
+                }
+ 
+                const promise = new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        if (age >= 18) {
+                            alert(`Welcome, ${name}. You can vote.`);
+                            resolve();
+                        } else {
+                            alert(`Oh sorry ${name}. You aren't old enough.`);
+                            reject();
+                        }
+                    }, 4000);
+                });
+ 
+                promise.catch(() => {
+                    // Handle promise rejection if needed
+                });
+            });
+        });
